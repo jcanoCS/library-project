877bb41ecfe114dbc61b7c4329f00772d87ad05c
Jose Cano <cano.5@wright.edu>
2023-12-05 16:37:47 -0500

    Book storing array and function to add to it

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Book Constructor
 function Book(title, author, numPages, genre, isRead=false) {
     this.title = title;
     this.author = author;
@@ -6,8 +7,17 @@ function Book(title, author, numPages, genre, isRead=false) {
     this.isRead = isRead;
 }
 
+// Adding a prototype function for every Book instance to display info
 Book.prototype.getInfo = function() {
     let readStatus = this.isRead ? 'reading completed.' : 'not read yet.';
     return `${this.title}, by ${this.author}, ${this.numPages} pages, ${this.genre}, ${readStatus}`
 };
 
+
+// Array to store Book instances
+const booksList = [];
+
+function addBookToLibrary(title, author, numPages, genre) {
+    newBook = new Book(title, author, numPages, genre);
+    booksList.push(newBook);
+};
